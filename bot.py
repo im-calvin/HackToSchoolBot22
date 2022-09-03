8fdcee11b4728a9f1e2078d002c6c2f92e28aa4c
Hob <93816506+HobbesChiong@users.noreply.github.com>
2022-09-03 16:53:36 -0700

    Question works now need to add listen for response

--- bot.py
+++ bot.py
@@ -30,15 +30,17 @@ async def on_message(message):
         msg = message.content[1:].split(' ')
         # command is the first word from the msg string
         command = msg[0].strip()
-        args = msg[1].strip()
+        if(len(msg) >=1):
+          args = msg[1:]
 
         if command == "hello":
             await message.channel.send('Hello!')
 
         if command == "askQuestion":
-            data = getReq()
-            len = data.length
-            index = random.randint(0,len)
+            # data = 
+            data = await getReq(message)
+            dataLen = len(data)
+            index = random.randint(0,dataLen)
             await message.channel.send(data[index]['ImageLink']) # this is the link to the image randomly choose object
             await message.channel.send('what is this?')
             # ans = data[index]['name'] is the answer to the question 
@@ -49,11 +51,12 @@ async def on_message(message):
 # gets data from the server and store into memory
 
 
-async def getReq():
-    URL = 'localhost:3000/api/getAll'  # url of the api where you're going to import
-    response = await requests.get(URL)
-    print(response.json())
-    return response.json
+async def getReq(message):
+    URL = 'http://localhost:3000/api/getAll'  # url of the api where you're going to import
+    response = requests.get(URL)
+    # later test for response.status_code works, prob dont have to but its good practice
+    print(response.status_code)
+    return response.json()
     
 
 client.run(DISCORD_TOKEN)
