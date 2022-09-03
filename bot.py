b046941112f31b5784057f6a54f943565b299d49
vinn03 <you@example.com/Users/vincentliu/Documents/HackToSchoolBot22/.venv/bin/python>
2022-09-03 16:49:49 -0700

    decode function added

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import os
 import random
 from dotenv import load_dotenv
 import requests
+import io
+import aiohttp
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
@@ -38,22 +40,38 @@ async def on_message(message):
         if command == "askQuestion":
             data = getReq()
             len = data.length
-            index = random.randint(0,len)
-            await message.channel.send(data[index]['ImageLink']) # this is the link to the image randomly choose object
+            await message.channel.send(decodeImg) # this is the link to the image randomly choose object
             await message.channel.send('what is this?')
+
             # ans = data[index]['name'] is the answer to the question 
             # response == ans 
             # send a message to the user saying if they got it right or wrong, exit while loop,
             # and once everything is finished work on leaderboard
 
+
 # gets data from the server and store into memory
 
 
 async def getReq():
-    URL = 'localhost:3000/api/getAll'  # url of the api where you're going to import
-    response = await requests.get(URL)
+    URL = ''  # url of the api where you're going to import
+    response = requests.get(URL)
     print(response.json())
     return response.json
+
+# decodes image url 
+async def decodeImg(message, datalist):
+
+    length = len(datalist)
+    index = random.randint(0,length)
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(data[index]['ImageLink']) as resp:
+            if resp.status != 200:
+                return await message.channel.send('Could not download file...')
+            data = io.BytesIO(await resp.read())
+            await message.channel.send(file=discord.File(data, 'cool_image.png'))
     
+
+
 
 client.run(DISCORD_TOKEN)
